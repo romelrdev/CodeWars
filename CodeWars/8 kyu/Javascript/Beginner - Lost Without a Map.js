3a63d919a4a23cb90d4ea92706b62b8b928ec99c
Romel Ramnarain Jr <78240340+romelrdev@users.noreply.github.com>
2021-02-17 21:42:01 -0500

    Add files via upload

---
+++ CodeWars/8 kyu/Javascript/Beginner - Lost Without a Map.js
@@ -0,0 +1,12 @@
+/*Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]*/
+
+//solution
+
+function maps(x){
+results = x.map(x => x * 2);
+  return results
+}
